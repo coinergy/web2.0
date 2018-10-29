805f94fa3b99b47f90d512e9c3e10a92ac0047a6
coinergy <coinergy.coinergy@gmail.com>
2018-10-29 13:24:37 +0800

    POST Input Setup

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.get('/',(req,res) =>{
-  res.send("HELLO WEB 2.0");
-})
+app.post('/',(req,res)=>{
+  var email = req.boady.email;
+  var amount = req.body.amount;
+
+  res.send({"amount" : amount . "email" : email });
+});
 
 app.listen(3000,()=>{
   console.log('server is running on port 3000');
